b4a9c075b9ada6b587083bb18cea189b378ecf33
andrm <minhnguyenn1311@gmail.com>
2024-12-20 18:05:56 -0500

    If applied, this commit will create the profile picture of Andrew Nguyen.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,18 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+
 function Hero() {
-  return <>Hero</>;
+  return (
+    <section id="hero">
+        <div className={styles.colorModeContainer}>
+            <img 
+                className={styles.hero}
+                src={heroImg} 
+                alt="Profile picture of Andrew Nguyen" 
+            />        
+        </div>
+    </section>
+  );
 }
  
 export default Hero;
