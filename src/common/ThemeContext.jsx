6a4fb7f718348e6d6f94c424cd3f0159ba9288e9
andrm <minhnguyenn1311@gmail.com>
2024-12-21 08:40:58 -0500

    If applied, this commit will help us toggle between different themes.

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -5,7 +5,16 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({children}) => {
-    const [theme, seTheme] = useState(
+    const [theme, setTheme] = useState(
         () => localStorage.getItem('theme') || 'light'
     );
+
+    useEffct(() => {
+        document.body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
 };
