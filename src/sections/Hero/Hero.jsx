00f6aa7f4f22135cb6b0ab1726cfc04989b57f61
andrm <minhnguyenn1311@gmail.com>
2024-12-20 20:36:26 -0500

    If applied, this commit will add the headers, color mode and social media icons.
    Added Andrew Nguyen and Software Developer headers; color mode, github and linkedin icons.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,15 +1,41 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
+import themeIcon from '../../assets/sun.svg';
+import githubIcon from '../../assets/github-light.svg';
+import linkedinIcon from '../../assets/linkedin-light.svg';
 
 function Hero() {
   return (
-    <section id="hero">
+    <section id="hero" className={styles.container}>
         <div className={styles.colorModeContainer}>
             <img 
                 className={styles.hero}
                 src={heroImg} 
                 alt="Profile picture of Andrew Nguyen" 
-            />        
+            />
+            <img 
+                className={styles.colorMode}
+                src={themeIcon}
+                alt="Color mode icon"
+            />
+        </div>
+        <div className={styles.info}>
+            <h1>
+                Andrew
+                <br />
+                Nguyen
+            </h1>
+            <h2>
+                Software Developer
+            </h2>
+            <span>
+                <a href="https://github.com/minhnguyen1311" target="_blank">
+                    <img src={githubIcon} alt="Github icon"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/andrew-nguyen-an2002/" target="_blank">
+                    <img src={linkedinIcon} alt="LinkedIn icon"></img>
+                </a>
+            </span>
         </div>
     </section>
   );
