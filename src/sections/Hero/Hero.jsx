cf13c6cf605492806050abc21298bf884404221e
andrm <minhnguyenn1311@gmail.com>
2024-12-23 16:06:36 -0500

    If applied, this commit will add the github and linkedin light and dark icons for toggle color switch.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,8 +2,10 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import githubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
 import CV from '../../assets/Resume-for-React.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
@@ -11,6 +13,8 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
     <section id="hero" className={styles.container}>
