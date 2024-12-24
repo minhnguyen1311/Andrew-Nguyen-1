2645f014f7a34af86aca5628f69e3d142e798b20
andrm <minhnguyenn1311@gmail.com>
2024-12-23 22:59:25 -0500

    If applied, this commit will create a ProjectCard file to hold the template of our projects.
    - Input MoonScape game screenshot to assets.
    - Create ProjectCard.jsx to fix redundancy.
    - Input multiple games with readability.

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,18 +1,26 @@
 import styles from './ProjectsStyles.module.css';
 import mentalSphere from '../../assets/mentalSphere.png';
+import moonScape from '../../assets/moonScape.png';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return ( 
     <section id="projects" className={styles.container}>
         <h1 className="sectionTitle"> Projects </h1>
         <div className={styles.projectsContainer}>
-            <a 
-                href="https://github.com/minhnguyen1311/MentalSphere"
-                target="_blank">
-                <img className="hover" src={mentalSphere} alt="Sphere logo" />
-                <h3>MentalSphere</h3>
-                <p>Interactive 3D Globe</p>
-            </a>
+          <ProjectCard 
+            src={mentalSphere} 
+            link="https://github.com/minhnguyen1311/MentalSphere" 
+            h3="MentalSphere"  
+            p="Interactive 3D Globe"
+          />
+
+          <ProjectCard 
+            src={moonScape} 
+            link="https://github.com/asapsui/MoonScape" 
+            h3="MoonScape"  
+            p="FPS Game"
+          />
         </div>
     </section>
   );
