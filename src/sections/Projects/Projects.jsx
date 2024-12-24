384208509eb8c0ee900f142e744136599a0bc803
andrm <minhnguyenn1311@gmail.com>
2024-12-23 21:17:30 -0500

    If applied, this commit will add the mentalSphere project.
    No css yet, just simple tags.

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import styles from './ProjectsStyles.module.css';
+import mentalSphere from '../../assets/mentalSphere.png';
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+  return ( 
+    <section id="projects" className={styles.container}>
+        <h1 className="sectionTitle"> Projects </h1>
+        <div ckassName={styles.projectsContainer}>
+            <a 
+                href="https://github.com/minhnguyen1311/MentalSphere"
+                target="_blank">
+                <img className="hover" src={mentalSphere} alt="Sphere logo" />
+                <h3>MentalSphere</h3>
+                <p>Interactive 3D Globe</p>
+            </a>
+        </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
