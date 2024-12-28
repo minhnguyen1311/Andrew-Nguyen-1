92003d94ae7b35cc0278d2e5c535278b3b87fbbe
andrm <minhnguyenn1311@gmail.com>
2024-12-27 23:32:02 -0500

    If applied, this commit will list all the skills in the skill section.

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,9 +1,34 @@
 import styles from './SkillsStyles.module.css';
+import checkMarkIcon from '../../assets/checkmark-dark.svg';
+import SkillList from '../../common/SkillList';
 
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
         <h1>Skills</h1>
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="HTML" />
+            <SkillList src={checkMarkIcon} skill="CSS" />
+            <SkillList src={checkMarkIcon} skill="JavaScript" />
+            <SkillList src={checkMarkIcon} skill="TypeScript" />
+            <SkillList src={checkMarkIcon} skill="Node" />
+        </div>
+        <hr />
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="React" />
+            <SkillList src={checkMarkIcon} skill="Angular" />
+            <SkillList src={checkMarkIcon} skill="Three" />
+            <SkillList src={checkMarkIcon} skill="Swift" />
+            <SkillList src={checkMarkIcon} skill="Git" />
+        </div>
+        <hr />
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="Python" />
+            <SkillList src={checkMarkIcon} skill="Java" />
+            <SkillList src={checkMarkIcon} skill="C++" />
+            <SkillList src={checkMarkIcon} skill="MATLAB" />
+            <SkillList src={checkMarkIcon} skill="Racket" />
+        </div>
     </section>
   );
 }
