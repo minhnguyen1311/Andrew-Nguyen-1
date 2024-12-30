6925b76bcc15e7240e1a103965196238f6c69b7f
andrm <minhnguyenn1311@gmail.com>
2024-12-29 19:15:50 -0500

    If applied, this commit will finish the footer html (no CSS yet).

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import styles from './FooterStyles.module.css';
 function Footer() {
   return (
     <section id="footer" className={styles.container}>
-        <p> &copy; </p>
+        <p> 
+            &copy; 2024 Andrew Nguyen. <br />
+            All rights reserved. 
+        </p>
     </section>
   );
 }
